09a84415f4827064db98fd66d09e1e6a274ce0c6
langzai2013 <1090108015.com>
2016-10-22 20:57:03 +0800

    修复 bug!

---
+++ app/src/main/java/com/zliang/shopping/bean/ShoppingCart.java
@@ -0,0 +1,37 @@
+package com.zliang.shopping.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by <a href="http://www.cniao5.com">菜鸟窝</a>
+ * 一个专业的Android开发在线教育平台
+ */
+public class ShoppingCart extends Ware implements Serializable {
+
+
+    private int count;
+    private boolean isChecked=true;
+
+
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public void setIsChecked(boolean isChecked) {
+        this.isChecked = isChecked;
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/zliang/shopping/utils/CartProvider.java
@@ -0,0 +1,91 @@
+package com.zliang.shopping.utils;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.SparseArray;
+
+import com.google.gson.reflect.TypeToken;
+import com.zliang.shopping.bean.ShoppingCart;
+import com.zliang.shopping.utils.JSONUtil;
+import com.zliang.shopping.utils.PreferencesUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/10/22 0022.
+ */
+public class CartProvider {
+
+
+    private SparseArray<ShoppingCart> datas;
+    private final String CART_JSON = "cart_json";
+    private Context mContext;
+
+    public CartProvider(Context context) {
+        mContext = context;
+        datas = new SparseArray<>(10);
+        listToSparse();
+    }
+
+    public void put(ShoppingCart cart) {
+
+        long carId = cart.getId();
+        ShoppingCart temp = datas.get(Integer.parseInt(String.valueOf(carId)));
+        if (temp != null) {
+            temp.setCount(temp.getCount() + 1);
+        } else {
+            temp = cart;
+            temp.setCount(1);
+        }
+        datas.put(Integer.parseInt(String.valueOf(carId)), temp);
+        commit();
+    }
+
+    public void update(ShoppingCart cart) {
+        datas.put(Integer.parseInt(String.valueOf(cart.getId())), cart);
+        commit();
+    }
+
+    public void delete(ShoppingCart cart) {
+        datas.delete(Integer.parseInt(String.valueOf(cart.getId())));
+        commit();
+    }
+
+    public List<ShoppingCart> getAll() {
+        return getDataFromLocal();
+    }
+
+    public void commit() {
+        List<ShoppingCart> carts = sparseToList();
+        PreferencesUtils.putString(mContext, CART_JSON, JSONUtil.toJSON(carts));
+    }
+
+    private void listToSparse() {
+        List<ShoppingCart> carts = getDataFromLocal();
+        if (datas != null && datas.size() > 0) {
+            for (ShoppingCart cart : carts) {
+                datas.put(Integer.valueOf(String.valueOf(cart.getId())), cart);
+            }
+        }
+    }
+
+    private List<ShoppingCart> sparseToList() {
+        int size = datas.size();
+        List<ShoppingCart> list = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            list.add(datas.valueAt(i));
+        }
+        return list;
+    }
+
+    public List<ShoppingCart> getDataFromLocal() {
+        String json = PreferencesUtils.getString(mContext, CART_JSON);
+        List<ShoppingCart> carts = null;
+        if (!TextUtils.isEmpty(json)) {
+            carts = JSONUtil.fromJson(json, new TypeToken<List<ShoppingCart>>() {
+            }.getType());
+        }
+        return carts;
+    }
+}
